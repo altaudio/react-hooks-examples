74d3a16f732ac423c7daf99652273b0e8c231ead
Alex Smith <altaudiolabs@gmail.com>
2019-09-22 13:48:04 +0100

    Makes useState example DRY

--- src/hooksExamples/stateHook.js
+++ src/hooksExamples/stateHook.js
@@ -1,5 +1,14 @@
 import React, { useState } from 'react';
 
+
+const Counter = ({ count, setCount, countName }) => (
+  <section>
+    <p>Count {countName}: {count}</p>
+    <button onClick={() => setCount(count + 1)}>Increment</button>
+    <button onClick={() => setCount(count - 1)}>Decrement</button>
+  </section>
+)
+
 export default () => {
   const [countOne, setCountOne] = useState(0);
   const [countTwo, setCountTwo] = useState(0);
@@ -10,23 +19,13 @@ export default () => {
       <h1>State Hook Examples</h1>
       <section>
 	<h2>Basic Count Example</h2>
-	<p>Count One: {countOne}</p>
-	<button onClick={() => setCountOne(countOne + 1)}>Increment</button>
-	<button onClick={() => setCountOne(countOne - 1)}>Decrement</button>
+	<Counter count={countOne} setCount={setCountOne} countName="One" />
       </section>
       <section>
 	<h2>Multiple useState Example</h2>
 	<p>Hooks must be used at the top level of the component, and <br/>not conditionally (eg. in an if statement, or a loop)</p>
-	<section>
-	  <p>Count Two: {countTwo}</p>
-	  <button onClick={() => setCountTwo(countTwo + 1)}>Increment</button>
-	  <button onClick={() => setCountTwo(countTwo - 1)}>Decrement</button>
-	</section>
-	<section>
-	  <p>Count Three: {countThree}</p>
-	  <button onClick={() => setCountThree(countThree + 1)}>Increment</button>
-	  <button onClick={() => setCountThree(countThree - 1)}>Decrement</button>
-	</section>
+	<Counter count={countTwo} setCount={setCountTwo} countName="Two" />
+	<Counter count={countThree} setCount={setCountThree} countName="Three" />
       </section>
     </section>
   )
